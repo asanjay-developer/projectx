1a88bfaf03e3a49d488a22546b0543a14053745a
asanjay-developer <sanjayselva6657@gmail.com>
2026-04-20 21:40:19 +0530

    Home page work done

---
+++ script.js
@@ -0,0 +1,11 @@
+var side_navbar = document.querySelector(".side-navbar")
+
+function show_side_navabar()
+{
+    side_navbar.style.left = "0"
+}
+
+function clos_side_navbar()
+{
+    side_navbar.style.left = "-60%"
+}
